c2b77c977b954d63391e536cb9b71708107a77e5
Tiisetso2004 <200781914+Tiisetso2004@users.noreply.github.com>
2026-03-10 17:38:14 +0200

    Refactor POE_part1 class for improved structure and readability; update user input handling and test reports

--- src/main/java/com/mycompany/poe_part1/POE_part1.java
+++ src/main/java/com/mycompany/poe_part1/POE_part1.java
@@ -3,64 +3,57 @@
  */
 
 package com.mycompany.poe_part1;
-import java. util.Scanner;
+
+import java.util.Scanner;
+
 /**
  *
  * @author RC_Student_lab
  */
 public class POE_part1 {
 
-// this is the main method where the scanner will be imported to prompt the user for input
-   
     public static void main(String args[]) {
-   Scanner scanner = new Scanner(System.in);    
-   
- 
-    
-// VARIABLES DECLARED AND PASSED ON TO CONSTRUCTOR USING THE SCANNER 
-    
-    String firstname = "";
-    String lastname = "";
-    String username = "";
-    String cellnumber = "";
-    String password = "";
-    String enteredUsername = "";
-    String enteredPassword = "";
+        Scanner scanner = new Scanner(System.in);
 
-// Instance of login class in the main method 
+        // Variables declared and passed on to constructor using the scanner
+        String firstname = "";
+        String lastname = "";
+        String username = "";
+        String cellnumber = "";
+        String password = "";
+        String enteredUsername = "";
+        String enteredPassword = "";
 
-Login login = new Login(username,cellnumber,password,firstname,lastname,enteredUsername,enteredPassword); //PARAMETERS ASSIGNED TO THE LOGIN CLASS 
+        // Instance of Login class
+        Login login = new Login(username, cellnumber, password, firstname, lastname, enteredUsername, enteredPassword);
 
-    firstname = login.checkFirstName(firstname); //Method handling entry of the firstname variable
+        firstname = login.checkFirstName(firstname);
+        lastname = login.checkLastName(lastname);
 
-    lastname = login.checkLastName(lastname);    //Method handling the entry lastname variable
+        // Loops which execute until the user enters correct details according to the prompts
 
+        do {
+            System.out.println("\nEnter your username: Username must equal five characters and contain an underscore");
+            username = scanner.nextLine();
+            login.registerUser(username);
+        } while (!login.checkUsername(username));
 
-//Loops which execute until the user enters correct details according to the prompts 
+        do {
+            System.out.println("\nEnter your cellnumber: Cellphone number must start with +27 and be 10 digits in length");
+            cellnumber = scanner.nextLine();
+            login.registerUser(cellnumber, username);
+        } while (!login.checkCellnumber(cellnumber));
 
-do{ System.out.println("\nEnter your username: Username must equal five characters and contain an underscore ");
-    username = scanner.nextLine();{login.registerUser(username);}}while(!login.checkUsername(username));
- 
+        do {
+            System.out.println("\nEnter your password: Password must equal 8 characters in length, contain a number, capital letter and a special character");
+            password = scanner.nextLine();
+            login.registerUser(password, username, cellnumber);
+        } while (!login.checkPassword(password));
 
-do{ System.out.println("\nEnter your cellnumber: Cellphone number must start with +27 and be 10 digits in length ");
-    cellnumber = scanner.nextLine();{login.registerUser(cellnumber,username);}}while(!login.checkCellnumber(cellnumber));
+        login.registerUser(username, cellnumber, password, firstname, lastname, true);
 
+        login.returnLoginStatus(enteredUsername, username, password, enteredPassword, firstname, lastname);
 
-
-do{ System.out.println("\nEnter your password: Password must equal 8 characters in length,contain a number,capital letter and contain a special character ");
-    password = scanner.nextLine();{login.registerUser(password,username,cellnumber);}}while(!login.checkPassword(password));
-
-
-//REGISTER USER METHOD INTERNALLY CALLS THE LOGIN USER METHOD 
-   
-    login.registerUser(username, cellnumber, password, firstname, lastname, true);
-   
-    login.returnLoginStatus(enteredUsername, username, password, enteredPassword, firstname, lastname); //RETURNS LOGIN STATUS
- 
-// END OF PROGRAM    
-    
-    scanner.close();  
-
-
+        scanner.close();
     }
 }
